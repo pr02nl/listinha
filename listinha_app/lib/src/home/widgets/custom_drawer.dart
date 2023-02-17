3888c84dcbe07fadbf4facaf17b41ed7c5eee885
Paulo Roberto <pr02nl@gmail.com>
2023-02-17 16:19:38 -0300

    feat: :sparkles: salva o estado de configuration localmente
    salva e recupera os dados de configuration utilizando o realm

--- listinha_app/lib/src/home/widgets/custom_drawer.dart
+++ listinha_app/lib/src/home/widgets/custom_drawer.dart
@@ -1,10 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_modular/flutter_modular.dart';
+import 'package:intl/intl.dart';
+
+import '../../shared/stores/app_store.dart';
 
 class CustomDrawer extends StatelessWidget {
   const CustomDrawer({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final appStore = context.watch<AppStore>(
+      (store) => store.syncDate,
+    );
+
+    final syncDate = appStore.syncDate.value;
+    var syncDateText = 'Nunca';
+
+    if (syncDate != null) {
+      final format = DateFormat('dd/MM/yyyy HH:mm');
+      syncDateText = format.format(syncDate);
+    }
+
     return NavigationDrawer(
       onDestinationSelected: (index) {
         if (index == 1) {
@@ -22,15 +38,18 @@ class CustomDrawer extends StatelessWidget {
         ),
         NavigationDrawerDestination(
           icon: const Icon(Icons.sync),
-          label: Row(
-            children: [
-              const Text('Sincronizar'),
-              const SizedBox(width: 28),
-              Text(
-                '11/02/2023 20:41',
-                style: Theme.of(context).textTheme.bodySmall,
-              ),
-            ],
+          label: SizedBox(
+            width: 210,
+            child: Row(
+              children: [
+                const Text('Sincronizar'),
+                const Spacer(),
+                Text(
+                  syncDateText,
+                  style: Theme.of(context).textTheme.bodySmall,
+                ),
+              ],
+            ),
           ),
         ),
         const NavigationDrawerDestination(
--- listinha_app/lib/src/shared/stores/app_store.dart
+++ listinha_app/lib/src/shared/stores/app_store.dart
@@ -1,7 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:listinha/src/configuration/services/configuration_service.dart';
 
 class AppStore {
   final themeMode = ValueNotifier<ThemeMode>(ThemeMode.system);
+  final syncDate = ValueNotifier<DateTime?>(null);
+
+  final ConfigurationService _configurationService;
+
+  AppStore(this._configurationService) {
+    init();
+  }
+
+  void init() {
+    final configuration = _configurationService.getConfiguration();
+    syncDate.value = configuration.syncDate;
+    themeMode.value = _getThemeModeByName(configuration.themeModeName);
+  }
+
+  void save() {
+    _configurationService.saveConfiguration(
+      themeMode.value.name,
+      syncDate.value,
+    );
+  }
+
   void changeThemeMode(ThemeMode? mode) {
     if (mode != null) {
       themeMode.value = mode;
@@ -9,7 +31,19 @@ class AppStore {
     }
   }
 
-  void save() {
-    //TODO(pr02nl): save theme mode
+  void setSyncDate(DateTime date) {
+    syncDate.value = date;
+    save();
+  }
+
+  void deleteAll() {
+    _configurationService.deleteAll();
+  }
+
+  ThemeMode _getThemeModeByName(String name) {
+    return ThemeMode.values.firstWhere(
+      (mode) => mode.name == name,
+      orElse: () => ThemeMode.system,
+    );
   }
 }
